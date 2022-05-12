60c68ddeb516a91a0c7e4931b4826f306781f626
MonicaHillman <mmhillman1@ucs.br>
2022-05-12 16:30:28 -0300

    ajustes nas funções assincronas

--- scripts/conexãoAPI.js
+++ scripts/conexãoAPI.js
@@ -1,21 +1,31 @@
 async function getPokemons(nome, endpoint) {
-
-    const API_POKEMON_URL = "https://pokeapi.co/api/v2/pokemon/";
-    const API_GENERATION_URL = "https://pokeapi.co/api/v2/generation/";
+    const API_URL = 'https://pokeapi.co/api/v2'
+    const API_POKEMON_URL = `${API_URL}/pokemon/`;
+    const API_GENERATION_URL = `${API_URL}/generation/`;
 
     if (nome == 'geração') {
-        document.querySelector(".pokemon").innerHTML = "";
-        const buscaApiGeração = await fetch(API_GENERATION_URL + endpoint).then((res) => res.json()).catch((err) => console.log("deu ruim!" + err))
-        const consultaApiPokemon = await buscaApiGeração.pokemon_species.map(pokemon => fetch(API_POKEMON_URL + pokemon.name).then(res => res.json()));
+        const buscaApiGeração = await fetch(API_GENERATION_URL + endpoint);
+        const converteApiGeração = await buscaApiGeração.json();
+        const consultaApiPokemon = await converteApiGeração.pokemon_species.map(async (pokemon) => {
+            const teste = await fetch(API_POKEMON_URL + pokemon.name);
+            return await teste.json();
+        });
         let resultado = await Promise.all(consultaApiPokemon);
         return resultado;
 
+
     } else {
-        const retornaPokemon = await fetch(API_POKEMON_URL + (endpoint || "")).then((res) => res.json()).catch((err) => "Não foi encontrado nenhum pokemon com esse nome!");
+        const retornaPokemon = await fetch(API_POKEMON_URL + (endpoint || ""));
+        const convertePokemon = await retornaPokemon.json();
+
         if (endpoint) {
-            return retornaPokemon;
+            return convertePokemon;
         } else {
-            const promises = await retornaPokemon.results.map(pokemon => fetch(pokemon.url).then(res => res.json()));
+            const promises = await convertePokemon.results.map(async (pokemon) => {
+                const verificaPokemon = await fetch(pokemon.url);
+                return await verificaPokemon.json();
+            });
+
             let resultadosConvertidos = await Promise.all(promises);
             return resultadosConvertidos;
         }
